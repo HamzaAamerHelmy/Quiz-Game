4a479e275a2a713bf1fa91303092f1bd51b89157
Hamza Aamer <hamza.aamer@outlook.com>
2026-08-06 13:39:17 +0300

    implement function to read number of questions from user input

--- main.cpp
+++ main.cpp
@@ -2,9 +2,17 @@
 #include <cstdlib>
 using namespace std;
 
+void readNumberOfQuestions(int &numOfQuestions)
+{
+    cout << "Enter Number Of Questions: ";
+    cin >> numOfQuestions;
+}
+
 int main()
 {
+    int numOfQuestions;
 
+    readNumberOfQuestions(numOfQuestions);
     
     return 0;
 }
