cecdb055e13f8654ac018f1003310dd16c7e6346
Hamza Aamer <hamza.aamer@outlook.com>
2026-08-06 13:48:37 +0300

    refactor: reorganize question level enum and improve readQuestionsLevel function

--- main.cpp
+++ main.cpp
@@ -2,17 +2,43 @@
 #include <cstdlib>
 using namespace std;
 
+enum enQuestionsLevel
+{
+    Easy = 1,
+    Med = 2,
+    Hard = 3,
+    Mix = 4
+};
+
 void readNumberOfQuestions(int &numOfQuestions)
 {
     cout << "Enter Number Of Questions: ";
     cin >> numOfQuestions;
 }
 
+enQuestionsLevel readQuestionsLevel(enQuestionsLevel questionsLevel)
+{
+    int userInput = 0;
+
+    cout << "Enter Questions Level [1]:Easy, [2]:Med, [3]:Hard, [4]:Mix : ";
+    cin >> userInput;
+
+    questionsLevel = (enQuestionsLevel)userInput;
+
+    return questionsLevel;
+}
+
 int main()
 {
+    srand((unsigned)time(NULL));
     int numOfQuestions;
 
+    
     readNumberOfQuestions(numOfQuestions);
+    
+    enQuestionsLevel questionsLevel = readQuestionsLevel(questionsLevel);
+
+    cout << questionsLevel << endl;
     
     return 0;
 }
